d582ccb3285fc643bc7905dfae63ba47db858dc7
sanix-darker <s4nixd@gmail.com>
2023-02-04 02:47:44 +0100

    feat: added reboot script to protect the sd card each day

---
+++ reboot/script.py
@@ -0,0 +1,20 @@
+import time
+from os import system
+from datetime import datetime
+
+
+def service():
+    now = datetime.now()
+    while True:
+        # if the hour is  between 1hour and 3hour,
+        # we are going to reboot
+        if 1 <= now.hour <= 3:
+            system("sudo reboot")
+
+        print(f"{now} : stand by...")
+        time.sleep(3600)
+
+
+if __name__ == "__main__":
+    print("reboot service started")
+    service()
