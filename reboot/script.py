f5cf6447890666058c2cf4cc7f2e1b4dd92e9eb3
sanix-darker <s4nixd@gmail.com>
2023-02-04 03:17:24 +0100

    fix: added the waiter before the command itself

--- reboot/script.py
+++ reboot/script.py
@@ -6,14 +6,15 @@ from datetime import datetime
 def service():
     now = datetime.now()
     while True:
-        time.sleep(30)
+        # we wait each hour
+        time.sleep(3600)
         # if the hour is  between 1hour and 3hour,
         # we are going to reboot
         if 1 <= now.hour <= 3:
             system("sudo reboot")
 
         print(f"{now} : stand by...")
-        time.sleep(3600)
+        time.sleep(30)
 
 
 if __name__ == "__main__":
