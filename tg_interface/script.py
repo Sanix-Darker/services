3d0d3541e01d63d17a7589eedf113a9c7e37d5ef
sanix-darker <s4nixd@gmail.com>
2023-02-05 13:39:54 +0100

    feat: implement streaming for output of running commands

--- tg_interface/script.py
+++ tg_interface/script.py
@@ -1,8 +1,12 @@
 import logging
-import subprocess
 from typing import List
 import configparser as Conf
+import asyncio
+from asyncio.subprocess import PIPE, create_subprocess_shell
+import nest_asyncio
+from functools import lru_cache
 
+from typing import Any
 from telegram.ext import Filters, MessageHandler, Updater
 from telegram.ext.utils.types import CCT
 from telegram.update import Update
@@ -13,14 +17,50 @@ configParser.read(r"config.txt")
 
 TELEGRAM_TOKEN: str = configParser.get("conf", "TELEGRAM_TOKEN")
 AUTHORIZED_USERS: List[str] = ["sanixdarker", "elhmn42"]
+_LOOP = asyncio.get_event_loop()
 
 
-def execute_cmd(_input: str):
-    return subprocess.getoutput(_input)
-    # return (
-    #     result.stdout.decode("utf-8") if result.stdout is not None else None,
-    #     result.stderr.decode("utf-8") if result.stderr is not None else None
-    # )
+# the printer could be
+async def run(command: str, printer: Any):
+    """
+    For commands that can take some time, i still want some ongoing output logs, stderr/stdout
+
+    Args:
+        - command: command line we want to execute
+        - printer: callback, we're going to use to output
+    """
+    global _LOOP
+
+    @lru_cache(maxsize=3)
+    async def _read_stream(stream, callback):
+        """ """
+        while True:
+            line = await stream.readline()
+            if line:
+                callback(line.decode("UTF8"))
+            else:
+                break
+
+    process = await create_subprocess_shell(
+        f"{command}", stdout=PIPE, stderr=PIPE, shell=True
+    )
+
+    out_task = _LOOP.create_task(
+        _read_stream(
+            process.stdout,
+            lambda x: printer(text=x)
+        )
+    )
+    err_task = _LOOP.create_task(
+        _read_stream(
+            process.stderr,
+            lambda x: printer(text=x)
+        )
+    )
+
+    await asyncio.wait({out_task, err_task})
+    await process.wait()
+
 
 def is_private(update: Update) -> bool:
     if update.message.chat.type == "private":
@@ -43,13 +83,12 @@ def exec_callback(update: Update, context: CCT) -> None:
             _command = update.message.text
             logging.info(f"{_command=}")
 
-            stdout = execute_cmd(_command)
-            context.bot.send_message(
-                chat_id=update.message.chat_id,
-                text=(
-                    f"::stdout::\n {stdout}\n\n"
-                )
+            msg = update.message.reply_text(
+                f"Running your command {_command}..."
             )
+
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(run(_command, msg.edit_text))
     except Exception as es:
         logging.exception(f"[x] Error {es}")
 
@@ -57,6 +96,7 @@ def exec_callback(update: Update, context: CCT) -> None:
 def set_callback() -> Updater:
     updater = Updater(token=TELEGRAM_TOKEN)
     dispatcher = updater.dispatcher
+
     dispatcher.add_handler(MessageHandler(Filters.text, exec_callback))
 
     return updater
@@ -64,6 +104,8 @@ def set_callback() -> Updater:
 
 if __name__ == "__main__":
     print("::tg-interface service started...")
+
+    nest_asyncio.apply()
 
     updater = set_callback()
     updater.start_polling()
